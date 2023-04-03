6fe19ead12fc440232cc9be4244d892f444d4c91
Bloogefest <bloogefest@gmail.com>
2023-04-03 20:55:37 +0300

    Введение аннотации `NotNull`

---
+++ src/main/java/com/bloogefest/annotation/analysis/NotNull.java
@@ -0,0 +1,23 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.bloogefest.annotation.analysis;
+
+import java.lang.annotation.*;
+
+/**
+ * Аннотация ненулевого значения.
+ *
+ * @apiNote Наличие этой аннотации говорит о том, что значение не должно быть нулевым.
+ * @since 1.0.0
+ */
+@Documented
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE,
+         ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NotNull {
+
+}
