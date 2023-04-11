9b8e694296b1bb9f04e2152a9d25ef756132b029
Bloogefest <bloogefest@gmail.com>
2023-04-11 11:45:26 +0300

    Введение аннотации `NotNls`

---
+++ src/main/java/com/bloogefest/annotation/analysis/NotNls.java
@@ -0,0 +1,23 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.bloogefest.annotation.analysis;
+
+import java.lang.annotation.*;
+
+/**
+ * Аннотация того, что не должно быть локализовано.
+ *
+ * @apiNote Наличие этой аннотации у чего-то говорит о том, что оно не должно быть локализовано.
+ * @since 1.0.0
+ */
+@Documented
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE,
+         ElementType.RECORD_COMPONENT})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NotNls {
+
+}
