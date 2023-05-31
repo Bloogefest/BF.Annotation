c45ab7f5db9372fa4899214fa28cac7d85913260
Bloogefest <bloogefest@gmail.com>
2023-05-31 14:17:34 +0300

    Введение аннотации `Enumed`

---
+++ src/main/java/com/bloogefest/annotation/analysis/Enumed.java
@@ -0,0 +1,62 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.bloogefest.annotation.analysis;
+
+import java.lang.annotation.*;
+
+/**
+ * Перечисление значений.
+ *
+ * @apiNote Уведомляет о предполагаемых значениях.
+ * @since 2.1.0-RC1
+ */
+@Documented
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.RECORD_COMPONENT})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Enumed {
+
+    /**
+     * Возвращает предполагаемые значения.
+     *
+     * @return Предполагаемые значения.
+     *
+     * @implSpec Предполагаемые значения не должны повторяться.
+     * @since 2.1.0-RC1
+     */
+    @Contract("-> const") int[] value() default {};
+
+}
