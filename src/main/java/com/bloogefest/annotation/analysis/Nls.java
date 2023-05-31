9558754be7c1e075c139459c6ab0fbc7908faf5c
Bloogefest <bloogefest@gmail.com>
2023-05-31 13:41:56 +0300

    Изменение документации аннотации `Nls`

--- src/main/java/com/bloogefest/annotation/analysis/Nls.java
+++ src/main/java/com/bloogefest/annotation/analysis/Nls.java
@@ -9,10 +9,10 @@ package com.bloogefest.annotation.analysis;
 import java.lang.annotation.*;
 
 /**
- * Аннотация того, что должно быть локализовано.
+ * Требование локализации.
  *
- * @apiNote Наличие этой аннотации у чего-то говорит о том, что оно должно быть локализовано.
- * @since 1.0.0
+ * @apiNote Позволяет подчеркнуть, что что-то должно быть локализовано.
+ * @since 1.0.0-RC1
  */
 @Documented
 @Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE,
