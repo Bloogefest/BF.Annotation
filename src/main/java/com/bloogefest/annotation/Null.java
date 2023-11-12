a7e097c8afeec569031d45e52c596f72a966c09d
Bloogefest <bloogefest@gmail.com>
2023-11-13 02:31:20 +0300

    Рефакторинг аннотации `Null`

--- src/main/java/com/bloogefest/annotation/Null.java
+++ src/main/java/com/bloogefest/annotation/Null.java
@@ -9,13 +9,19 @@ package com.bloogefest.annotation;
 import java.lang.annotation.*;
 
 /**
- * Нулевое значение.
+ * Позволяет обозначить значение элемента как нулевое. Эта аннотация применима к элементам, значение которых должно быть
+ * нулевым.
  *
- * @apiNote Позволяет подчеркнуть, что значение должно быть нулевым.
+ * @apiNote Эта аннотация активно изменялась, вплоть до выпуск-кандидата 3.0.0-RC1, после чего планируется допускать
+ * лишь небольшие изменения в её документации.
+ * @implSpec Реализация этой аннотации по умолчанию является эталонной, так как она полностью следует <a
+ * href="https://annotation.docs.bloogefest.com/reference/null#specification">спецификации</a>.
+ * @implNote Для того чтобы применение этой аннотации было максимально эффективно, следуйте <a
+ * href="https://annotation.docs.bloogefest.com/reference/null#recommendations">рекомендациям по применению</a>.
  * @since 1.0.0-RC1
  */
 @Documented
-@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE,
-         ElementType.TYPE_USE})
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE,
+         ElementType.RECORD_COMPONENT})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Null {}
